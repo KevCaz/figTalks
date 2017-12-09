8bdca069b220513b1509698166fccda31346e0e5
Kevin Cazelles <kcazelle@uoguelph.ca>
2017-12-09 13:54:09 -0500

    add processes figures

--- R/classicTIB.R
+++ R/classicTIB.R
@@ -34,7 +34,7 @@ showtext_auto()
 figMW <- function(filename = "img/figMW", part = 1, hg = 5, wi = 8.5, res = 300,
   col1 = '#787878', col2 = "#1b95e0", col3 = '#232323'){
   ##
-  cex.l <- 5
+  cex.l <- 6
   filename <- paste0(filename, part, ".png")
   ##
   png(filename, height = hg, width = wi, unit="in", res = res)
@@ -42,7 +42,6 @@ figMW <- function(filename = "img/figMW", part = 1, hg = 5, wi = 8.5, res = 300,
     layout(matrix(c(1,2), ncol=2), width = c(1,0.24))
 
     par(
-      cex=1.5,
       cex.lab = 4,
       cex.axis = 4,
       las = 1,
@@ -50,9 +49,9 @@ figMW <- function(filename = "img/figMW", part = 1, hg = 5, wi = 8.5, res = 300,
       bg = "transparent",
       fg= col1,
       col.axis = col1,
-      family="arya",
-      mar = c(1.5,0.5,1,0),
-      mgp=c(2.2,1,0)
+      family = "arya",
+      mar = c(2.5, 1.5, 2, 0),
+      mgp = c(2.4, 1, 0)
     )
 
     ##--- Main plot region
@@ -62,8 +61,8 @@ figMW <- function(filename = "img/figMW", part = 1, hg = 5, wi = 8.5, res = 300,
 	    if (part>6) lines(A, C[,2L], lwd=3, col=col1, lty=4)
 	    if (part>4) lines(A, B[,3L], lwd=3, col=col2)
 	    if (part>5) lines(A, C[,3L], lwd=3, col=col2, lty=4)
-      mtext(1, text="Specific richness", cex=cex.l, line=.2)
-      mtext(3, at=-0.5, text="Rates", cex=cex.l, line=.1)
+      mtext(1, text="Specific richness", cex=cex.l, line=.5)
+      mtext(3, at=-0.5, text="Rates", cex=cex.l, line=.25)
       box(bty="l")
 
       if (part>7) {
@@ -74,11 +73,11 @@ figMW <- function(filename = "img/figMW", part = 1, hg = 5, wi = 8.5, res = 300,
 
       if (part>4){
         if (part<6){
-          legend(5, 0.5, c("Colonisation"), lty=1, ncol=2, bty="n",
-          seg.len=1, cex=2.4)
+          legend(0, 0.53, c("Colonisation"), lty=1, ncol=2, bty="n",
+          seg.len=1.2, cex=4, lwd=2)
         } else {
-          legend(5, 0.5, c("Colonisation", "Extinction"), lty=c(1,4), ncol=2, bty="n",
-          seg.len=1, cex=2.4)
+          legend(0, 0.53, c("Colonisation", "Extinction"), lty=c(1,4), ncol=2, bty="n",
+          seg.len=1.2, cex=4, lwd=2)
         }
       }
     }
@@ -87,12 +86,12 @@ figMW <- function(filename = "img/figMW", part = 1, hg = 5, wi = 8.5, res = 300,
     plot0(c(0,10),c(0,10))
     abline(h=8, lwd=2, col=col1)
     rect(-1,8,11,11, col=col1, border=NA)
-    text(5, 9.25, "Continent", cex=cex.l*.7, col=col3)
+    text(5, 9.25, "Continent", cex=cex.l, col=col3)
 
     if (part>1){
     rect(.5, 6.5, 6, 5.25, col=col2, border=NA)
     if (part>2) arrows2(x0=mean(c(.5,6)), y0=8, y1=6.5, col=col1, border=NA, prophead=FALSE)
-    text(3.25, 5.875, "Island", cex=cex.l*.7, col=col3)
+    text(3.25, 5.875, "Island", cex=cex.l*.9, col=col3)
     }
 
     if (part>6) {
